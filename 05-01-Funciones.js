c3574db916e25bdcc0358fd0cf3ef198419ce390
piaolivera <piaoliveraa@gmail.com>
2024-02-09 16:22:56 -0300

    FUNCION DE TIPO FLECHA

--- 05-01-Funciones.js
+++ 05-01-Funciones.js
@@ -1,6 +1,6 @@
 
 // DECLARACION DE LA FUNCION.
-function miFuncion(a, b){
+function miFuncion(a, b){ 
     return a + b;
 }
 
@@ -10,12 +10,20 @@ let resultado = miFuncion(2, 3);
 console.log(resultado);
 
 //Declaracion de una funcion tipo expresion 
-let x = function (a, b){return a + b};
-resultado = x(1,2);
+let sumar = function (a, b){return a + b};
+resultado = sumar(1,2);
 console.log(resultado);
 
 (function(a, b){
     console.log("Ejecutando la funcion: " +(a+b));
 })(3, 4); // con los parentesis llamamos la funcion y se ejecuta. en el caso
 // de no colocar los parentesis, no se ejecuta el console.log.
+
+
+
+//funcion flecha
+const sumarFuncionTipoFlecha = (a, b) => a + b;
+resultado = sumarFuncionTipoFlecha(3, 5);
+console.log(resultado);
+
 

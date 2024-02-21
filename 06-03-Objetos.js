b63c2bdbf10d8101b450b72154257cf4fa4e6cec
piaolivera <piaoliveraa@gmail.com>
2024-02-21 12:12:53 -0300

    Distintas maneras de Imprimir Objectos

--- 06-03-Objetos.js
+++ 06-03-Objetos.js
@@ -1,1 +1,24 @@
 // Concatenar cada valor de cada propiedad
+let persona = {
+    nombre : 'ROBERTO',
+    apellido: 'Campos',
+    email: 'campos@gmail.com',
+    edad: 28,
+    nombreCompleto: function(){ //ESTO ES UN MÉTODO/FUNCIÓN
+        return this.nombre +' '+ this.apellido;
+    }
+}
+
+console.log(persona.nombre + ', '+ persona.apellido);
+
+//for in
+for (nombrePropiedad in persona){
+    console.log(persona[nombrePropiedad]);
+}
+
+let personaArray =Object.values(persona);
+console.log(personaArray);
+
+let personaString = JSON.stringify(persona);
+console.log(personaString);
+

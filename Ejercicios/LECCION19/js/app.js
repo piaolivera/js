2a3cdbab544346b4c434375d8f2c14f3c7215df0
Pía Olivera <107521407+piaolivera@users.noreply.github.com>
2024-05-03 20:46:21 -0300

    js

---
+++ Ejercicios/LECCION19/js/app.js
@@ -0,0 +1,25 @@
+const personas = [
+    new Persona('Juan', 'Perez'),
+    new Persona ('Maria', 'Gimenez')
+];
+
+function mostrarPersonas(){
+    console.log('Mostrar personas...');
+    let texto = ' ';
+    for(let persona of personas){
+        console.log(persona);
+        texto +=`<li>${persona.nombre} ${persona.apellido} </li>`;
+    }
+    document.getElementById('personas').innerHTML = texto;
+}
+
+
+function agregarPersona(){
+    const forma = document.forms('forma');
+    const nombre = forma['nombre'];
+    const apellido = forma['apellido'];
+    const persona = new Persona(nombre.value, apellido.value);
+    console.log(persona)
+    personas.push(persona);
+    mostrarPersonas();
+}
